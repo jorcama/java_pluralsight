ffa9961c74a692289b358cd4f31c9f02217e4297
jorcama <jorcama@gmail.com>
2024-11-11 15:44:19 +0100

    controller

---
+++ contacts/src/main/java/com/dubhe/pluralsight/contacts/ContactRepository.java
@@ -0,0 +1,7 @@
+package com.dubhe.pluralsight.contacts;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+}
